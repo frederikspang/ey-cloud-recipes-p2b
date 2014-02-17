641ab69a443f2972b54b830b661674a042d119a2
Rasmus Brøndsted <rbb@place2book.com>
2014-02-17 14:01:41 +0100

    disable install pdftk

--- cookbooks/appcore/recipes/default.rb
+++ cookbooks/appcore/recipes/default.rb
@@ -5,17 +5,17 @@
 
 require_recipe "appcore::cron"
 
-enable_package "app-text/pdftk-bin" do
-  version "1.44"
-end
-
-package "app-text/pdftk-bin" do
-  action :install
-end
-
-remote_file "/usr/bin/pdftk" do
-  owner "root"
-  group "root"
-  mode 0755
-  source "pdftk"
-end
+#enable_package "app-text/pdftk-bin" do
+#  version "1.44"
+#end
+#
+#package "app-text/pdftk-bin" do
+#  action :install
+#end
+#
+#remote_file "/usr/bin/pdftk" do
+#  owner "root"
+#  group "root"
+#  mode 0755
+#  source "pdftk"
+#end
