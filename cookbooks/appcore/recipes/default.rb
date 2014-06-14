c6ab2217ee19a721874cb0d1e845564f2b2d05fc
Peter Sandager Jeppesen <psjeppesen@gmail.com>
2014-06-14 03:33:00 +0200

    Static path

--- cookbooks/appcore/recipes/default.rb
+++ cookbooks/appcore/recipes/default.rb
@@ -6,6 +6,6 @@
 require_recipe "appcore::cron"
 
 execute "ensure required directories" do
-  command "mkdir -p /data/#{app}/shared/locales"
-  command "mkdir -p /data/#{app}/shared/storage"
+  command "mkdir -p /data/appcore/shared/locales"
+  command "mkdir -p /data/appcore/shared/storage"
 end
