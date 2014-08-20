de3fba06a57a50ecdd4b5fd92c5f4914d0ce18b1
Peter Sandager Jeppesen <psjeppesen@gmail.com>
2014-08-21 01:59:00 +0200

    Added recipe for custom unicorn setup

---
+++ cookbooks/unicorn/recipes/default.rb
@@ -0,0 +1,37 @@
+#Set your application name here
+appname = "appcore"
+
+service "unicorn_#{appname}" do
+  supports :restart => true
+end
+
+template "/data/#{appname}/shared/config/env.custom" do
+  source "env.custom.erb"
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0644
+  backup 0
+  variables({
+    :appname => appname
+  })
+end
+
+case node[:ec2][:instance_type]
+when 'm1.small', 'm3.small'  then worker_count = 5
+when 'm1.medium', 'm3.medium', 'c1.medium' then worker_count = 10
+when 'm1.large', 'm3.large', 'c1.large', 'c3.large'  then worker_count = 20
+when 'm1.xlarge', 'm3.xlarge', 'c1.xlarge', 'c3.xlarge' then worker_count = 40
+else worker_count = 10
+end
+
+remote_file "/data/#{appname}/shared/config/custom_unicorn.rb" do
+  source "custom_unicorn.erb"
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0644
+  backup 0
+  variables({
+    :worker_count => worker_count
+  })
+  notifies :restart, resources(:service => "unicorn_#{appname}")
+end
