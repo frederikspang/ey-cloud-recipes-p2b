92653a596cdadd93503d8487746b1ce31de3c9f4
psj <psjeppesen@gmail.com>
2014-11-13 10:38:55 +0100

    Enable and install updated ImageMagick in emerge recipe

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -6,3 +6,12 @@ package "app-misc/ca-certificates" do
   version "20120623"
   action :install
 end
+
+enable_package "media-gfx/imagemagick" do
+  version "6.7.8.8"
+end
+
+package "media-gfx/imagemagick" do
+  version "6.7.8.8"
+  action :install
+end
