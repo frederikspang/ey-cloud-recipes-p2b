bc303aab8cd9fc021046c7e608fd7b6dfc281ebf
psj <psjeppesen@gmail.com>
2015-02-09 12:24:18 +0100

    Change ImageMagick version

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -8,10 +8,10 @@ package "app-misc/ca-certificates" do
 end
 
 enable_package "media-gfx/imagemagick" do
-  version "6.7.8.8"
+  version "6.7.8.7"
 end
 
 package "media-gfx/imagemagick" do
-  version "6.7.8.8"
+  version "6.7.8.7"
   action :install
 end
