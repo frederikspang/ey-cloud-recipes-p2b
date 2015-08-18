9bdb3c6b620976a92f78624d289454b6e84dddc5
psj <psjeppesen@gmail.com>
2015-08-18 13:18:48 +0200

    Update OpenSSL version

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -8,5 +8,6 @@
 
 default[:packages] = [
   {:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.2.1"},
-  {:name => "media-fonts/corefonts", :version => "1-r4"}
+  {:name => "media-fonts/corefonts", :version => "1-r4"},
+  {:name => "dev-libs/openssl", :version => "1.0.1p"}
 ]
