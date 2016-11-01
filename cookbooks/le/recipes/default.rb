b291cae127c4289aa635e9db7b778cd80fdb6805
Peter Sandager Jeppesen <psjeppesen@gmail.com>
2016-11-01 02:28:41 +0100

    le recipe only for app_master

--- cookbooks/le/recipes/default.rb
+++ cookbooks/le/recipes/default.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: le
 # Recipe:: default
 #
-require_recipe 'le::install'
-require_recipe 'le::configure'
-require_recipe 'le::start'
+if node[:environment][:framework_env] == "production" && ['app_master'].include?(node[:instance_role])
+  require_recipe 'le::install'
+  require_recipe 'le::configure'
+  require_recipe 'le::start'
+end
