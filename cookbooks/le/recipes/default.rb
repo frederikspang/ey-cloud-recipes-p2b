791f5cebd7ec23048df234ede33284229e72523b
Peter Sandager Jeppesen <psjeppesen@gmail.com>
2017-02-03 02:37:52 +0100

    Revert "le recipe only for app_master"
    This reverts commit b291cae127c4289aa635e9db7b778cd80fdb6805.

--- cookbooks/le/recipes/default.rb
+++ cookbooks/le/recipes/default.rb
@@ -2,8 +2,6 @@
 # Cookbook Name:: le
 # Recipe:: default
 #
-if node[:environment][:framework_env] == "production" && ['app_master'].include?(node[:instance_role])
-  require_recipe 'le::install'
-  require_recipe 'le::configure'
-  require_recipe 'le::start'
-end
+require_recipe 'le::install'
+require_recipe 'le::configure'
+require_recipe 'le::start'
